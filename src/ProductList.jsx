c9c7a07079b5359c493050e1df2c9d37a12ad2f9
Carlos <88097124+felimaker@users.noreply.github.com>
2026-07-29 18:13:57 -0500

    Create ProductList component for plant listings
    Added a new ProductList component for displaying plants.

---
+++ src/ProductList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+function ProductList() {
+  return (
+    <div className="product-list-page">
+      <h2>Our Plants</h2>
+      <div className="products-grid">
+        {/* The product listing page of the plant shop application */}
+        <p>List of beautiful plants will be rendered here...</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;
